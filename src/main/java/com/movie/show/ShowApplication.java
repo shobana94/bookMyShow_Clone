182c96a1ea1d64fadd1ac0bfed389b8cd327ad2d
shobana94 <shobana.v@iamneo.ai>
2024-04-22 13:15:50 +0530

    added File

---
+++ src/main/java/com/movie/show/ShowApplication.java
@@ -0,0 +1,13 @@
+package com.movie.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowApplication.class, args);
+	}
+
+}
